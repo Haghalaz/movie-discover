db0b25d521ad43e80fff8dc81b9a44b1e6dcc80b
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-05-07 00:42:10 -0300

    refactor: Change the function parameters

--- src/utils/RandomizeMovie/index.ts
+++ src/utils/RandomizeMovie/index.ts
@@ -8,10 +8,10 @@ function getRandomItemsFromArray(arr: MovieResume[], count: number): MovieResume
   return shuffled.slice(0, count); // Return the first 'count' elements
 }
 
-export default function RandomizeMovies(gender: string): MovieResume[] {
-  const randomPage = Math.floor(Math.random() * 10) + 1;
+export default function RandomizeMovies(gender: string) {
+  const randomPage = Math.floor(Math.random() * 20) + 1;
 
-  const { response } = useAxios(
+  const { response, isLoading } = useAxios(
     `/discover/movie?page=${randomPage}&include_adult=false&include_video=false&language=pt-BR&page=1&sort_by=popularity.desc&with_genres=${gender}`
   );
 
@@ -20,5 +20,5 @@ export default function RandomizeMovies(gender: string): MovieResume[] {
     return getRandomItemsFromArray(response?.results, 6);
   }, [response]);
 
-  return randomized();
+  return { data: randomized(), isLoading };
 }
